da1dd712115fbbfb200b76fcca8b86d457ff5b41
Nicola Elia <nicolaelia94@gmail.com>
2020-12-11 20:29:24 +0100

    Comments review

--- examples/Advanced/OTASketchDownloadWifi/arduino_secrets.h
+++ examples/Advanced/OTASketchDownloadWifi/arduino_secrets.h
@@ -1,3 +1,3 @@
-// Insert your WiFi secrets here
-#define SECRET_SSID "MY-SSID"
-#define SECRET_PASS "MY-PASSWORD"
+// Insert your WiFi secrets here:
+#define SECRET_SSID "" // Your network SSID (name)    
+#define SECRET_PASS "" // Your network password
